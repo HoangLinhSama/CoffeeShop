885c2569b4cb674022ced213b88fadec509f7f69
Hoang Linh Sama <88495638+HoangLinhSama@users.noreply.github.com>
2024-05-20 23:27:39 +0700

    Revert "Add connection to server"

--- Server/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "coffee_shop";
-$connect = mysqli_connect($hostname, $username, $password, $database);
-mysqli_query($connect, "SET NAMES 'utf8'");
-?>
